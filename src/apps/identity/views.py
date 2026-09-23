9c656e10044d1f69d8cd2b5987b35f61e9b165bf
antenyijoseph20-glitch <antenyijoseph20@gmail.com>
2026-09-23 14:01:54 +0100

    learn: explore Django request object

--- src/apps/identity/views.py
+++ src/apps/identity/views.py
@@ -5,6 +5,9 @@ def home(request):
     context = {
         "platform_name": "NCD Patient Safety Platform",
         "message": "Patient safety, care coordination and support.",
+        "method": request.method,
+        "path": request.path,
+        "name": request.GET.get("name"),
     }
 
     return render(request, "home.html", context)
